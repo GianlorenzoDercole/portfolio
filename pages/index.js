83cd29f56358cd189abdaa53e9ada7bdd6ee78f5
GianlorenzoDercole <johndercole01@gmail.com>
2022-07-26 20:30:58 -0700

    removed apostrophe from name

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ export default function Home() {
             margin-right: 20px
         }
     `}</style>
-<h1 className='whiteText margin-left'>John D'Ercole</h1>
+<h1 className='whiteText margin-left'>John Dercole</h1>
 
 <h4 className='orangeText margin-left'>Full Stack Software Engineer</h4>
 <div className='bg mt'>
